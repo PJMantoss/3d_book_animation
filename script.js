4a3591c5ccc7d471272f62ecea181f51c7c0dae7
PJMantoss <joelptoss@gmail.com>
2022-12-09 18:37:05 +0100

    update script

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function bookOpen(){
     let page = document.getElementsByClassName("page");
     let index = page.length;
+
+    page[index - 1].getElementsByClassName.transform = "rotateY(180deg)";
 }
