325bce659a02e70f6b4df9192ec7fa606fb98824
PJMantoss <joelptoss@gmail.com>
2022-12-14 18:09:01 +0100

    update script

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ function bookOpen(){
 
     page[index - 1].style.transform = "rotateY(180deg)";
     page[index - 1].style.boxShadow = "0 0 8px #fff";
+    page[index - 2].style.boxShadow = "0 0 8px #fff";
+    page[index - 3].style.boxShadow = "0 0 8px #fff";
+    page[index - 4].style.boxShadow = "0 0 8px #fff";
+    page[index - 5].style.boxShadow = "0 0 8px #fff";
+    page[index - 6].style.boxShadow = "0 0 8px #fff";
 
     setTimeout(() => {}, 400);
 }
